1091ea0f0280baa0a1dea25dd700db931f7bb953
Ryochi <ryo1.t.zubu@gmail.com>
2021-11-18 23:38:50 +1000

    Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: []
+}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return(
+                {...state, 
+                    firnends: [
+                        ...state.friends,
+                        action.firnend
+                    ]
+                }
+            )
+        case "REMOVE_FRIEND":
+            const removalIndex = state.friends.findIndex(friend => firnend.id === action.id);
+            return (
+                {...state,
+                    friends: [
+                        ...state.friends.slice(0, removalIndex),
+                        ...state.friends.slice(removalIndex + 1)
+                    ]
+                }
+            )
+        default:
+            return state;
+    }
 }
